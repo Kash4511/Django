627ee6b64ff021cea461e1cae577ea4e02693837
Kash <Kaashifameen32@gmail.com>
2026-02-08 21:39:46 +0530

    testing login error (2nd feb)

--- Backend/accounts/urls.py
+++ Backend/accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 from .views import UserRegistrationView, UserLoginView, UserProfileView
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='user-register'),
     path('login/', UserLoginView.as_view(), name='user-login'),
     path('profile/', UserProfileView.as_view(), name='user-profile'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
